c6e3515ea9c17ff202dad25d99d0956ce3c55d18
dhahn <you@example.com>
2021-01-08 19:04:18 +0100

    bump version

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ use clap::{App, AppSettings, Arg, SubCommand};
 use client::start_client;
 use server::start_server;
 
-const VERSION: &str = "0.0.7";
+const VERSION: &str = "0.0.8";
 
 fn main() {
     pretty_env_logger::init();
