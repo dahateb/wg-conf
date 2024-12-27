45267157cbf6cc770f914a3e87d6d9cd4c026030
dan <daniel.hahn06@gmail.com>
2024-12-27 16:59:41 +0100

    update dependencies

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -1,21 +1,21 @@
-use base64::{decode, encode};
+use base64::{Engine as _, engine::general_purpose};
 use rand_core::OsRng;
 use x25519_dalek::{PublicKey, StaticSecret};
 
 //add error handling
 pub fn get_public_key(private_key: &str) -> String {
-    let decoded_bytes = decode(private_key).unwrap();
+    let decoded_bytes = general_purpose::STANDARD.decode(private_key).unwrap();
     let mut private_bytes = [0u8; 32];
     private_bytes[..decoded_bytes.len()].copy_from_slice(&decoded_bytes);
     let secret = StaticSecret::from(private_bytes);
     let pub_key = PublicKey::from(&secret);
-    encode(pub_key.as_bytes())
+    general_purpose::STANDARD.encode(pub_key.as_bytes())
 }
 
 pub fn generate_key_pair() -> (String, String) {
-    let secret = StaticSecret::new(OsRng);
-    let private_key = encode(secret.to_bytes());
-    let public_key = encode(PublicKey::from(&secret).as_bytes());
+    let secret = StaticSecret::random_from_rng(OsRng);
+    let private_key = general_purpose::STANDARD.encode(secret.to_bytes());
+    let public_key = general_purpose::STANDARD.encode(PublicKey::from(&secret).as_bytes());
     (private_key, public_key)
 }
 
